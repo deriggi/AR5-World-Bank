b14dbf2f6d1b5531408c8574e2fe98d8c71fb1b3
deriggi <john.deriggi@gmail.com>
2014-05-16 00:33:10 -0400

    raster stacker attacker!

--- rasterStacker.R
+++ rasterStacker.R
@@ -1,30 +1,53 @@
 
 # allfiles <- list all files
-rootDir <- "F:/climate/monthly/pr/monthtrend_20"
-allFiles <- list.files(rootDir, full.names=TRUE)
-innerParts <- list()
+library(raster)
+yearsies <- c(20, 40, 60, 80)
+climatevars <- c("pr", "tas", "tasmin", "tasmax")
+for (cv in climatevars){
 
-i <- 0
+	for (year in yearsies){
+		rootDir <- paste("F:/climate/monthly/", cv, "/monthtrend_", year, sep="")
 
-for (file in allFiles){
-	part <- unlist(strsplit(file, '_'))
-	part <- paste(part[3], part[4], part[5], part[6], part[7], sep='_')
-	# part <- part[2:6]
-	if( !(part %in% innerParts) ){
-		innerParts[i] <- part
-		write(part, stdout())
-		i <- i +1
+		allFiles <- list.files(rootDir, full.names=TRUE)
+		innerParts <- list()
 
+		i <- 0
+
+		for (file in allFiles){
+			part <- unlist(strsplit(file, '_'))
+			part <- paste(part[3], part[4], part[5], part[6], part[7], sep='_')
+			# part <- part[2:6]
+			if( !(part %in% innerParts) ){
+				innerParts[i] <- part
+				write(part, stdout())
+				i <- i +1
+
+			}
+		}
+
+		for (part in innerParts){
+			pat = paste(".*", part, ".*\\.tif$", sep="")
+			allFiles <- list.files(rootDir, full.names=TRUE, pattern=pat)
+			
+			# sort this, should be 12
+			sort(allFiles)
+			if(length(allFiles) != 12){
+				write(" not 12!", stdout())
+				write(allFiles, stdout())
+				write('', stdout())	
+
+			}else{
+				stizzy <- stack()
+				for(rasty in allFiles){
+					stizzy <- addLayer(stizzy, rasty)
+				}
+				destinationfolder <- paste("F:/climate/monthly/",cv,"/monthtrendstacked_", year, "/" , sep="")
+				dir.create(destinationfolder)
+				writeRaster(stizzy, format="GTiff", filename=paste(destinationfolder, part, ".tif",  sep="") )
+			}
+		}
 	}
 }
-
-for (part in innerParts){
-	pat = paste(".*", part, ".*\\.tif$", sep="")
-	allFiles <- list.files(rootDir, full.names=TRUE, pattern=pat)
-	write(allFiles, stdout())
-	write('', stdout())	
-}
-
 
 ## TODO, use python to rename all files so they are 01 instead of 1 on months
 # v <- get inner part of all in allFiles
