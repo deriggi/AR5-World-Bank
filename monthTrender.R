f1ac05a689c81980a6fe1392b5cdbfada7292850
deriggi <john.deriggi@gmail.com>
2014-09-09 07:58:10 -0400

    monthTrender stacks as default

--- monthTrender.R
+++ monthTrender.R
@@ -1,33 +1,50 @@
+
 library(raster)
-cvar <- 'tasmax'
-years <- c(20,40,60,80)
-for (year in years){
-	rootDir <- paste("D:/climate/monthly/",cvar,"/outgeotiff_", year , "_rotated_reprojected_regridded_nd/",sep="")
-	write(rootDir, stdout())
-	output_folder <- paste("F:/climate/monthly/",cvar,"/monthtrend_", year ,  '/', sep="")
-	path_length <- nchar(rootDir)
-	allFiles <- list.files(rootDir, full.names=TRUE, pattern="\\.tif$")
 
-	for (file in allFiles){
-		
-		months = seq(1,12)
-		
-		for (month in months){
-			myStack <- stack()	
+monthTrend <- function() {
+	cvars <- c('tasmax', 'tas', 'pr')
+	years <- c(1965, 1985)
+	for (cvar in cvars)
+	{
+		for (year in years)
+		{
+			rootDir <- paste("F:/climate/historical/",cvar,"/outgeotiff_", year , "_rotated/",sep="")
+			write(rootDir, stdout())
+			output_folder <- paste("F:/climate/historical/",cvar,"/monthtrend_", year ,  '/', sep="")
+			dir.create(output_folder)
 
-			factors = seq(0,19)
+			path_length <- nchar(rootDir)
+			allFiles <- list.files(rootDir, full.names=TRUE, pattern="\\.tif$")
 
-			for (factor in factors){
-				# write(month + factor*12,stdout())
-				band <- month + factor*12
-				myStack <- addLayer(myStack,raster(file, band=band))
+			for (file in allFiles)
+			{
 
+				months = seq(1,12)
+				outerStack<- stack()
+
+				for (month in months){
+					myStack <- stack()	
+
+					factors = seq(0,19)
+
+					for (factor in factors){
+					# write(month + factor*12,stdout())
+					band <- month + factor*12
+					myStack <- addLayer(myStack,raster(file, band=band))
+
+				}
+				write( paste('nlayers: ', nlayers(myStack), sep="") , stdout())
+
+				monthRaster <- stackApply(myStack, c(1), mean ) 
+				outerStack <- addLayer(outerStack, monthRaster)
 			}
-			write( paste('nlayers: ', nlayers(myStack), sep="") , stdout())
-			outfilename<-substr(file, path_length+1, nchar(file)-4)
 
-			stackApply(myStack, c(1), mean, filename=paste( output_folder , outfilename, '_' , month , '.tif', sep="") ) 
+			outfilename<-substr(file, path_length+1, nchar(file)-4)
+			writeRaster(outerStack, filename=paste( output_folder , outfilename, '.tif', sep=""))
+			# write outerStack to file here
+
 		}
 	}
 }
-
+}
+monthTrend()
